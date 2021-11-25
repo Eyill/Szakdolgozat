06cf41ee6f8db92f234d620f17680dc6108eabce
Pokrovszkaja Katerina <p.katerina@freemail.hu>
2021-11-25 17:23:13 +0100

    Delete target directory

--- target/classes/adventure/entities/Player.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Player" tilewidth="16" tileheight="16" tilecount="1152" columns="32">
- <image source="16x16 knight 1.png" trans="ffffff" width="512" height="576"/>
-</tileset>
